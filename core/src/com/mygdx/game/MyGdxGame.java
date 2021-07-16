c536d21c5dd7f19d2e858e85ad1cc3ab8e891b79
leanderson123 <leandersondeoliveira@hotmail.com>
2021-07-16 13:25:13 -0300

    limapndo estrutra e extendendo GameBase

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,32 +1,15 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+public class MyGdxGame extends GameBeta {
 
-public class MyGdxGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void initialize() {
+
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void update(float dt) {
+
 	}
 }
